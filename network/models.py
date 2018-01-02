c07702e2787888e21d758dafe192b447634b7b3f
Max Ungless <maxungless@gmail.com>
2018-01-02 15:03:39 +0000

    Added Post model

--- network/models.py
+++ network/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    published = models.DateTimeField(default=timezone.now)
